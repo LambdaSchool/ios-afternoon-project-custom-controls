0cdf9cde5a677396688cfd755772df1469f22782
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-14 13:37:27 -0700

    setup()  , intrinsicContentSize

--- Custom-Controls/Custom-Controls/CustomControl.swift
+++ Custom-Controls/Custom-Controls/CustomControl.swift
@@ -24,14 +24,28 @@ class CustomControl: UIControl {
 	private func setup() {
 		var labels: [UILabel] = []
 		
-		for i in 1...5 {
+		for i in 0...4 {
 			let label = UILabel()
-			label.tag = i
+			//let x = i == 0 ?  8.0  * CGFloat(i) :  8.0 + componentDimension * CGFloat(i)
+			let frame =  CGRect(x: 8.0 + componentDimension * CGFloat(i), y: 0, width: componentDimension, height: componentDimension)
+			label.frame = frame
+			label.tag = i + 1
+			label.font = UIFont.boldSystemFont(ofSize: 32)
+			//let darkstar = Character(★)
+			label.text = "☆"
+			label.textColor = .green
 			labels.append(label)
 		}
 		
 		labels.forEach( { addSubview( $0 )} )
 		print(labels.count)
+	}
+	
+	override var intrinsicContentSize: CGSize {
+		let componentsWidth = CGFloat(componentCount) * componentDimension
+		let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+		let width = componentsWidth + componentsSpacing
+		return CGSize(width: width, height: componentDimension)
 	}
 	
 	override func draw(_ rect: CGRect) {
