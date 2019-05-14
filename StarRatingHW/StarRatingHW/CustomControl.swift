84119a8e69f61756d65a5c54820a0afe19c98a67
Michael R.F <mrflowers22@uwalumni.com>
2019-05-14 14:50:41 -0400

    implemented the updateRatings function

---
+++ StarRatingHW/StarRatingHW/CustomControl.swift
@@ -0,0 +1,14 @@
+//
+//  CustomControl.swift
+//  StarRatingHW
+//
+//  Created by Michael Flowers on 5/14/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    var value: Int = 1
+    
+}
