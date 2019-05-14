58f4cc800116c07feb09bdc52a100c742c46df83
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-14 17:10:27 -0400

    Add labels

--- CustomControls-Rating/CustomControls-Rating/Custom Controls and View Controllers/CustomControl.swift
+++ CustomControls-Rating/CustomControls-Rating/Custom Controls and View Controllers/CustomControl.swift
@@ -12,9 +12,42 @@ import UIKit
     
     // MARK : - Properties
     var value: Int = 1
+    var labels = [UILabel]()
+    
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount = 5
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
     
     // MARK: - View Loading
     
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        
+        setup()
+    }
+    
+    func setup() {
+        for index in 1...componentCount {
+            let x = CGFloat(index - 1)*(componentDimension + 8.0)
+            let newLabel = UILabel(frame: CGRect(x: x, y: 0.0, width: componentDimension, height: componentDimension))
+            newLabel.tag = index
+            newLabel.font = UIFont.boldSystemFont(ofSize: 32.0)
+            newLabel.text = "✰"
+            newLabel.textAlignment = .center
+            newLabel.textColor = index == 1 ? componentActiveColor : componentInactiveColor
+            
+            addSubview(newLabel)
+            labels.append(newLabel)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
     
     // MARK: - Touch Handling
     
