37c75ab8ac8f4572ce8428c5e33b3bd2ec998485
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-14 17:20:30 -0400

    Set up touch tracking

--- CustomControls-Rating/CustomControls-Rating/Custom Controls and View Controllers/CustomControl.swift
+++ CustomControls-Rating/CustomControls-Rating/Custom Controls and View Controllers/CustomControl.swift
@@ -50,6 +50,39 @@ import UIKit
     }
     
     // MARK: - Touch Handling
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        return true
+    }
     
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDragInside])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        return true
+    }
     
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer { super.endTracking(touch, with: event) }
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchUpInside])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    func updateValue(at touch: UITouch) {
+        
+    }
 }
