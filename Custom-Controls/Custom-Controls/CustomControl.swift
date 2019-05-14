f7d8de2627880378de4d49a752a2f52437bc4c19
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-14 12:13:24 -0700

    init

---
+++ Custom-Controls/Custom-Controls/CustomControl.swift
@@ -0,0 +1,21 @@
+//
+//  CustomControl.swift
+//  Custom-Controls
+//
+//  Created by Hector Steven on 5/14/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+
+	
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
