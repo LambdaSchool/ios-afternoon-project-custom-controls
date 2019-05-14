f1a570a9ef103d4f2e8fa104ec7d2bee4a642469
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-14 16:48:42 -0400

    Set up app skeleton

---
+++ CustomControls-Rating/CustomControls-Rating/Custom Controls and View Controllers/CustomControl.swift
@@ -0,0 +1,22 @@
+//
+//  CustomControl.swift
+//  CustomControls-Rating
+//
+//  Created by Lisa Sampson on 5/14/19.
+//  Copyright © 2019 Lisa M Sampson. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomControl: UIControl {
+    
+    // MARK : - Properties
+    var value: Int = 1
+    
+    // MARK: - View Loading
+    
+    
+    // MARK: - Touch Handling
+    
+    
+}
---
+++ CustomControls-Rating/CustomControls-Rating/Custom Controls and View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomControls-Rating
+//
+//  Created by Lisa Sampson on 5/14/19.
+//  Copyright © 2019 Lisa M Sampson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
