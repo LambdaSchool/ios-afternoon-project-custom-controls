ddde5f554a3268653b1237addcadb684f9a5eef7
Christopher Aronson <christopheraronson@icloud.com>
2019-05-14 11:49:54 -0700

    Init Commit

---
+++ Start/Start/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Start
+//
+//  Created by Christopher Aronson on 5/14/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
