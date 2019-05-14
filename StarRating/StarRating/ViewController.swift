8fe2097d2b5e195b29a7761f0d8c4de9422cd1e6
Sameera Leola <pixelandbyte@gmail.com>
2019-05-14 13:51:11 -0500

    Initial commit after setting up project

---
+++ StarRating/StarRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by Sameera Roussi on 5/14/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
