8a32b648c1a2d721de715864cec9ac99028ae792
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-21 13:01:01 -0600

    Initial commit
    Started with a new Xcode project.

---
+++ Custom Controls/Custom Controls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom Controls
+//
+//  Created by Mitchell Budge on 5/21/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
