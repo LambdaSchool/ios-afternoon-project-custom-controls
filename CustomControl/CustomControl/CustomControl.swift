c1a8a2b0857b0503b69f20423f45064eed862aad
Jeremy Taylor <jtaylor@jctaylor.org>
2019-05-21 14:19:33 -0500

    Initial setup done

---
+++ CustomControl/CustomControl/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  CustomControl
+//
+//  Created by Jeremy Taylor on 5/21/19.
+//  Copyright © 2019 Bytes-Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    var value = 1
+}
