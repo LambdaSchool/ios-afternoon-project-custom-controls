0c0d4e7788af6fa053872cadf239baa925fafec2
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-28 14:50:28 -0400

    Built the control view.

---
+++ CustomControls/CustomControls/CustomControl.swift
@@ -0,0 +1,15 @@
+//
+//  CustomControl.swift
+//  CustomControls
+//
+//  Created by Thomas Cacciatore on 5/28/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+    
+}
