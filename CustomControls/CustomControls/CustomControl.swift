80c8c3022a01e926cc24eb9cc865b5fc405aa516
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-28 17:36:47 -0400

    Built control view.  Previous commit created custom class.

--- CustomControls/CustomControls/CustomControl.swift
+++ CustomControls/CustomControls/CustomControl.swift
@@ -12,4 +12,63 @@ class CustomControl: UIControl {
     
     var value: Int = 1
     
+    let componentDimension: CGFloat = 40.0
+    let componentCount = 5
+    let componentActiveColor: UIColor = .black
+    let componentInactiveColor: UIColor = .gray
+    
+    
+    func setup() {
+        var labels: [UILabel] = []
+        let spacing: CGFloat = 8
+        var xPosition: CGFloat = spacing
+        let yPosition: CGFloat = 0
+        for i in 0...componentCount - 1 {
+            let label = UILabel()
+            label.backgroundColor = .orange
+            addSubview(label)
+            label.tag = i + 1
+            labels.append(label)
+            let frameSize = CGSize(width: componentDimension, height: componentDimension)
+            label.frame.size = frameSize
+            label.frame.origin = CGPoint(x: xPosition, y: yPosition)
+            xPosition += componentDimension + spacing
+            label.font = UIFont.boldSystemFont(ofSize: 32.0)
+            label.text = "☆"
+            label.textAlignment = .center
+            if label.tag == 1 {
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
+            
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        return true
+    }
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        return true
+    }
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        
+    }
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        setup()
+        }
+    
+    
 }
