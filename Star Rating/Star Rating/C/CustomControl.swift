709730a758e1705af62db5b6c87c5c50d2c5cc67
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-13 16:03:32 -0400

    Initial. Sets up UIView on Storyboard. Created Custom Control class and action.

---
+++ Star Rating/Star Rating/C/CustomControl.swift
@@ -0,0 +1,21 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Nathan Hedgeman on 8/13/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomControl : UIControl {
+    
+    //Properties
+    var value: Int = 1
+}
+
+//MARK: - Funtions
+extension CustomControl {
+
+}
---
+++ Star Rating/Star Rating/C/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Star Rating
+//
+//  Created by Nathan Hedgeman on 8/13/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        
+        
+        
+    }
+}
+
+
+
+
+
