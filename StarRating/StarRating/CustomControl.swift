42217d1a361b2db895d43b58a99afca08a2fc749
bradleyyin <bradleyyin@gmail.com>
2019-08-13 19:59:56 -0700

    implement custom control appearance

--- StarRating/StarRating/CustomControl.swift
+++ StarRating/StarRating/CustomControl.swift
@@ -11,4 +11,41 @@ import UIKit
 
 class CustomControl: UIControl {
     var value = 1
+    
+    private let componentDimension : CGFloat = 40.0
+    private let componentCount = 5
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setup()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
+    
+    func setup() {
+        var labels: [UILabel] = []
+        var xPostion: CGFloat = 8.0
+        for i in 1...5 {
+            let newLabel = UILabel(frame: CGRect(x: xPostion, y: 0.0, width: componentDimension, height: componentDimension))
+            xPostion += (componentDimension + 8.0)
+            newLabel.tag = i
+            newLabel.font = newLabel.font.withSize(32.0)
+            newLabel.text = "☆"
+            newLabel.textAlignment = .center
+            if i == 1 {
+                newLabel.textColor = componentActiveColor
+            } else {
+                newLabel.textColor = componentInactiveColor
+            }
+            self.addSubview(newLabel)
+            labels.append(newLabel)
+        }
+    }
 }
