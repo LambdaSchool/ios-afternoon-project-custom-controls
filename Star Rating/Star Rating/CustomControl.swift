8e9deffca1dba87466fd90b5cdb86e7d0ca5d5df
Alex Shillingford <alex.shillingford@icloud.com>
2019-09-12 14:27:19 -0600

    Create CustomControl class, updateRating @IBAction, and set up Storyboard UIView

---
+++ Star Rating/Star Rating/CustomControl.swift
@@ -0,0 +1,23 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Alex Shillingford on 9/12/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Star Rating/Star Rating/ViewController.swift
+++ Star Rating/Star Rating/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        if ratingControl.value == 1 {
+            self.title = "User Rating: N star"
+        } else {
+            self.title = "User Rating: N stars"
+        }
+    }
+    
 }
 
