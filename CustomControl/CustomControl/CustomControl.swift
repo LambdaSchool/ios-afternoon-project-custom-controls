97fcb2f68cf01c804901b80068ec05aeeabe73c8
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-12 14:43:40 -0700

    Created new custom control class

---
+++ CustomControl/CustomControl/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  CustomControl
+//
+//  Created by Taylor Lyles on 9/12/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+	var value: Int = 1
+}
