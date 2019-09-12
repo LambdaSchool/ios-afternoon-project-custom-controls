3aa608c10e2b45276e21f4c3e46b90ac5cbe5690
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-12 15:00:03 -0700

    Added component constants

--- CustomControl/CustomControl/CustomControl.swift
+++ CustomControl/CustomControl/CustomControl.swift
@@ -9,5 +9,10 @@
 import UIKit
 
 class CustomControl: UIControl {
+	
 	var value: Int = 1
+	let componentDimension: CGFloat = 40.0
+	let componentCount = 5
+	let componentActiveColor = UIColor.black
+	let componentInactiveCOlor = UIColor.gray
 }
