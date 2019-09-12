63f86077b6038ddd8b503656581409777721b763
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-12 15:12:27 -0700

    Added for loop to build 5 labels

--- CustomControl/CustomControl/CustomControl.swift
+++ CustomControl/CustomControl/CustomControl.swift
@@ -15,4 +15,16 @@ class CustomControl: UIControl {
 	let componentCount = 5
 	let componentActiveColor = UIColor.black
 	let componentInactiveCOlor = UIColor.gray
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		setup()
+	}
+	
+	func setup() {
+		for i in 1...componentCount {
+			let label = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 40, height: 40)))
+		}
+	}
+	
 }
