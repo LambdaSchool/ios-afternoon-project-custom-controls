6ebd14372d020ab96d1ad922a302fc0cd04d94d5
Andrew Ruiz <nukedoom@gmail.com>
2019-09-12 13:43:57 -0700

    Testing

---
+++ Star Rating/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Andrew Ruiz on 9/12/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    var value : Int = 1
+}
