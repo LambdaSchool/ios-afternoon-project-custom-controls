80dc816fabc3740eda41754d49927aca8ec76e65
Eoin Lavery <dev@tenantc.co.uk>
2019-09-26 10:37:55 +0100

    Initial Commit

---
+++ Custom Controls/Custom Controls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom Controls
+//
+//  Created by Eoin Lavery on 26/09/2019.
+//  Copyright © 2019 Eoin Lavery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
