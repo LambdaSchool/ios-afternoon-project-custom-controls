6d2e3200c79074114f5005f4af8a5f24dc4e3061
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-26 17:39:12 -0400

    initial commit

---
+++ Star Rating/Star Rating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Rating
+//
+//  Created by Vici Shaweddy on 9/26/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
