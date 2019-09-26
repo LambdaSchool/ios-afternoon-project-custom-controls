1e15f4c29612aac65c1dba5d9cfc3eccf4377bb9
Eoin Lavery <dev@tenantc.co.uk>
2019-09-26 11:17:56 +0100

    Built CustomControl view

--- Custom Controls/Custom Controls/CustomControl.swift
+++ Custom Controls/Custom Controls/CustomControl.swift
@@ -11,8 +11,49 @@ import UIKit
 class CustomControl: UIControl {
     
     //MARK: - Properties
+    //Rating property
     var value: Int = 1
     
+    //Component Properties
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount: Int = 5
+    private let componentActiveColor: UIColor = .black
+    private let componentInactiveColor: UIColor = .gray
     
+    //Store UILabel items
+    private var labelArray: [UILabel] = []
+    
+    //MARK: - Initializer
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    //MARK: - Private Functions
+    private func setup() {
+        for i in 1...componentCount {
+            let label = UILabel()
+            if i == 1 {
+                label.frame = CGRect(x: 8.0, y: 0.0, width: componentDimension, height: componentDimension)
+            } else {
+                let currentLabelPosition = CGFloat( (componentDimension * CGFloat(i - 1) + CGFloat( i * 8 )) )
+                label.frame = CGRect(x: currentLabelPosition, y: 0.0, width: componentDimension, height: componentDimension)
+            }
+            label.font = UIFont(name: "System-Bold", size: 32.0)
+            label.text = "✰"
+            label.textAlignment = .center
+            label.tag = i
+            if i == 1 {label.textColor = componentActiveColor} else { label.textColor = componentInactiveColor}
+            addSubview(label)
+            labelArray.append(label)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
     
 }
