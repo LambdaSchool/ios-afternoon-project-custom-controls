fcf5e0fc3c21388bafca96ee870ce1481d562e96
Robert Keffury <bobby.keffury@me.com>
2019-09-26 09:43:29 -0700

    Added Stars

--- StarRating/StarRating/CustomControl.swift
+++ StarRating/StarRating/CustomControl.swift
@@ -13,4 +13,83 @@ class CustomControl: UIControl {
     
     var value: Int = 1
     
+    var stars: [UILabel] = []
+   
+    
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount: CGFloat = 5.0
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setup()
+    }
+    
+    func setup() {
+        
+            
+        let star1 = UILabel(frame: CGRect(x: 8.0, y: 0, width: componentDimension, height: componentDimension))
+        let star2 = UILabel(frame: CGRect(x: componentDimension + 16.0, y: 0, width: componentDimension, height: componentDimension))
+        let star3 = UILabel(frame: CGRect(x: componentDimension + 64.0, y: 0, width: componentDimension, height: componentDimension))
+        let star4 = UILabel(frame: CGRect(x: componentDimension + 112.0 , y: 0, width: componentDimension, height: componentDimension))
+        let star5 = UILabel(frame: CGRect(x: componentDimension + 160.0, y: 0, width: componentDimension, height: componentDimension))
+            
+            
+            
+        star1.tag = 1
+        star2.tag = 2
+        star3.tag = 3
+        star4.tag = 4
+        star5.tag = 5
+            
+            
+        star1.font = UIFont(name: "bold system font", size: 32.0)
+        star2.font = UIFont(name: "bold system font", size: 32.0)
+        star3.font = UIFont(name: "bold system font", size: 32.0)
+        star4.font = UIFont(name: "bold system font", size: 32.0)
+        star5.font = UIFont(name: "bold system font", size: 32.0)
+            
+        star1.text = "☆"
+        star2.text = "☆"
+        star3.text = "☆"
+        star4.text = "☆"
+        star5.text = "☆"
+            
+        star1.textAlignment = .center
+        star2.textAlignment = .center
+        star3.textAlignment = .center
+        star4.textAlignment = .center
+        star5.textAlignment = .center
+            
+        star1.textColor = componentActiveColor
+        star2.textColor = componentInactiveColor
+        star3.textColor = componentInactiveColor
+        star4.textColor = componentInactiveColor
+        star5.textColor = componentInactiveColor
+            
+        addSubview(star1)
+        addSubview(star2)
+        addSubview(star3)
+        addSubview(star4)
+        addSubview(star5)
+            
+        stars.append(star1)
+        stars.append(star2)
+        stars.append(star3)
+        stars.append(star4)
+        stars.append(star5)
+    }
+    
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
+    
+    
+    
 }
