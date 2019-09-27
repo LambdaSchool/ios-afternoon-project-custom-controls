a8e4b0da3f1857673cc5041e48327c0c11a08d56
Joel Groomer <joelgroomer@gmail.com>
2019-09-26 19:13:03 -0700

    Section "Create a new custom control class" complete.

---
+++ Star Rating/Star Rating/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Joel Groomer on 9/26/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    var value: Int = 1
+}
