ac8503beb50a007227ac10ccded90bc81da9998c
John Kouris <jdkouris@gmail.com>
2019-09-26 18:04:35 -0700

    Created the skeleton Xcode project

---
+++ StarRating/StarRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by John Kouris on 9/26/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
