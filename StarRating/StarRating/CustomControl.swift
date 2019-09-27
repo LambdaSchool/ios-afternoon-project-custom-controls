9c7712089efd22b4213e3f25eada9e1d3589f832
John Kouris <jdkouris@gmail.com>
2019-09-26 18:46:14 -0700

    Finished with the instructions. Need to fix layout and the star selection

--- StarRating/StarRating/CustomControl.swift
+++ StarRating/StarRating/CustomControl.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
+@IBDesignable
 class CustomControl: UIControl {
     
     static var value: Int = 1
-    private let componenetDimension: CGFloat = 40.0
+    private let componentDimension: CGFloat = 40.0
     private let componentCount = 5
     private let componentActiveColor = UIColor.black
     private let componentInactiveColor = UIColor.gray
+    
+    var labelArray = [UILabel]()
     
     required init?(coder aCoder: NSCoder) {
         super.init(coder: aCoder)
@@ -23,24 +26,74 @@ class CustomControl: UIControl {
     }
     
     func setup() {
-        var labelArray = [UILabel]()
-        
         for i in 1...5 {
             let label = UILabel()
             label.tag = i
             addSubview(label)
+            label.frame.size = CGSize(width: componentDimension, height: componentDimension)
+            let x = componentDimension + CGFloat(i * 8)
+            label.center = CGPoint(x: x, y: 0)
+            label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+            label.text = "⭑"
+            label.textAlignment = .center
+            if i == 1 {
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
             labelArray.append(label)
         }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
+    
+    func updateValue(at touch: UITouch) {
+        for label in labelArray {
+            if label.frame.contains(touch.location(in: self)) {
+                self.value(forKey: "label.tag")
+                label.textColor = componentActiveColor
+                sendActions(for: [.valueChanged])
+            }
+        }
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDragInside])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer { super.endTracking(touch, with: event) }
         
+        guard let touch = touch else { return }
         
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchUpInside])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
     }
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
     }
-    */
-
+    
 }
