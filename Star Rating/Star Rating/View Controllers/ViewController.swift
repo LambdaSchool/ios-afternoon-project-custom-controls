52537a3321cbcad41bc83eea3e43e2f83399ab9b
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-10-10 13:45:42 -1000

    Project Setup

---
+++ Star Rating/Star Rating/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Star Rating
+//
+//  Created by Jesse Ruiz on 10/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
