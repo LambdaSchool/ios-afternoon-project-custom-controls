1e65a2b0bbc29d009653bec7f0b7a6335d1d2336
Brandi <blubrandi0@gmail.com>
2019-11-07 14:45:45 -0700

    Create the Custom Control class and added a view and action and title.

---
+++ CustomControls/CustomControls/CustomControl.swift
@@ -0,0 +1,16 @@
+//
+//  CustomControl.swift
+//  CustomControls
+//
+//  Created by BDawg on 11/7/19.
+//  Copyright © 2019 Brandi. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+    
+    
+}
--- CustomControls/CustomControls/ViewController.swift
+++ CustomControls/CustomControls/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
