46f797e47fac549ef518d2f7dde0dcd9b0fe609a
Jon Bash <jonbash@protonmail.com>
2019-11-07 12:34:42 -0800

    implement control setup

--- Star Rating/Star Rating/CustomControl.swift
+++ Star Rating/Star Rating/CustomControl.swift
@@ -9,5 +9,50 @@
 import UIKit
 
 class CustomControl: UIControl {
+    
+    // MARK: - Properties
+    
     var value: Int = 1
+    
+    let componentCount: Int = 5
+    let componentDimension: CGFloat = 40
+    let componentSpaceInterval: CGFloat = 8
+    let componentActiveColor = UIColor.black
+    let componentInactiveColor = UIColor.gray
+    //let unselectedComponentText = "☆"
+    let componentText = "★"
+    
+    // MARK: - Init
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        var starLabels = [UILabel]()
+        for i in 1...componentCount {
+            let starLabel = UILabel()
+            
+            starLabel.translatesAutoresizingMaskIntoConstraints = false
+            starLabel.frame = CGRect(
+                x: CGFloat(i) * componentSpaceInterval, y: 0,
+                width: componentDimension, height: componentDimension
+            )
+            starLabel.tag = i
+            starLabel.font = .boldSystemFont(ofSize: 32)
+            starLabel.text = componentText
+            starLabel.textColor = i == 1 ? componentActiveColor : componentInactiveColor
+            
+            addSubview(starLabel)
+            starLabels.append(starLabel)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * componentSpaceInterval
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
 }
