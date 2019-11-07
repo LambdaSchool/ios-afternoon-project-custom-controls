e9958c2bb9b710f03df686346d2dac8acb702fc8
Jon Bash <jonbash@protonmail.com>
2019-11-07 13:22:16 -0800

    tweak flare animation

--- Star Rating/Star Rating/CustomControl.swift
+++ Star Rating/Star Rating/CustomControl.swift
@@ -124,11 +124,11 @@ class CustomControl: UIControl {
 extension UIView {
     // "Flare view" animation sequence
     func performFlare() {
-        func flare()   { transform = CGAffineTransform(scaleX: 1.6, y: 1.6) }
+        func flare()   { transform = CGAffineTransform(scaleX: 1.2, y: 1.2) }
         func unflare() { transform = .identity }
         
         UIView.animate(
-            withDuration: 0.3,
+            withDuration: 0.1,
             animations: { flare() },
             completion: { _ in UIView.animate(withDuration: 0.1) { unflare() }})
     }
