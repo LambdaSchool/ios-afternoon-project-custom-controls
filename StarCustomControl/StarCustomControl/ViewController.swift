8d4a437957b92ecf5aedc92dc24966d4f9eb124e
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-11-07 11:54:11 -0800

    Added StarCustomControl Project

---
+++ StarCustomControl/StarCustomControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarCustomControl
+//
+//  Created by Lambda_School_Loaner_204 on 11/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
