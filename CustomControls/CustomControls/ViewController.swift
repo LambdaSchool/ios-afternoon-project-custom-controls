bd4a8c749ad675e2e4c1b581682a8fa3370217c1
Dan M <djmthedev@gmail.com>
2019-11-07 16:23:13 -0800

    done with a stretch

--- CustomControls/CustomControls/ViewController.swift
+++ CustomControls/CustomControls/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    let customControl = CustomControl()
+    
+    // MARK: - Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.title = "User Rating: \(customControl.value) stars"
     }
-
-
+    
+    // MARK: - Actions
+    
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        // FIXME: - stars/star based on 1/or more
+        print("hi")
+        navigationItem.title = "User Rating: \(ratingControl.value) stars"
+    }
+    
 }
 
