1714965aeae224f14494fdf665bb602c39094868
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-23 21:39:51 +0000

    Tasks in 'Create a new custom class' done

---
+++ StarRating/StarRating/CustomControl.swift
@@ -0,0 +1,17 @@
+//
+//  CustomControl.swift
+//  StarRating
+//
+//  Created by Tobi Kuyoro on 23/01/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    // MARK: - Properties
+    
+    var value: Int = 1
+    
+}
