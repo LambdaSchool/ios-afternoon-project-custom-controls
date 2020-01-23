8b81064be952c03f16b1d6df1d64ea050ca7797d
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-23 15:57:15 -0800

    done

--- CustomControlsAP/CustomControlsAP/CustomControl.swift
+++ CustomControlsAP/CustomControlsAP/CustomControl.swift
@@ -17,10 +17,14 @@ class CustomControl: UIControl {
     
     private let componentDimension: CGFloat = 40.0
     private let componentCount = 5
-    private let componentActiveColor = UIColor.black
-    private let componentInactiveColor = UIColor.lightGray
+    private let componentActiveColor = UIColor.systemYellow
+    private let componentInactiveColor = UIColor.systemGray6
     
-    var value: Int = 1
+    var value: Int = 1 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
     var labelArray: [UILabel] = []
     
     required init?(coder: NSCoder) {
@@ -37,10 +41,12 @@ class CustomControl: UIControl {
             label.frame = CGRect(x: (componentDimension * CGFloat(i - 1)) + CGFloat(8 * i), y: 0, width: componentDimension, height: componentDimension)
             if i == 1 {
                 label.textColor = componentActiveColor
+                label.text = "★"
             } else {
                 label.textColor = componentInactiveColor
+                label.text = "☆"
             }
-            label.text = "☆"
+            
             label.textAlignment = .center
             label.font = UIFont.systemFont(ofSize: 32) // Change later?
             //label.font = UIFont(name: "System-Bold", size: 32)
@@ -54,4 +60,77 @@ class CustomControl: UIControl {
       let width = componentsWidth + componentsSpacing
       return CGSize(width: width, height: componentDimension)
     }
+    
+    // MARK: - Touch Hanlders
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        //let touchPoint = touch.location(in: self)
+        updateValue(at: touch)
+        sendActions(for: [.touchDown, .touchUpInside, .valueChanged])
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            updateValue(at: touch)
+            sendActions(for: [.touchDragInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer { super.endTracking(touch, with: event) }
+        guard let touch = touch else { return }
+        
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            
+            sendActions(for: [.touchUpInside])
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    func updateValue(at touch: UITouch) {
+        print("updateValue called")
+        let touchPoint = touch.location(in: self)
+        
+        for label in labelArray {
+            if label.frame.contains(touchPoint) {
+                self.value = label.tag
+                for lab in labelArray {
+                    if lab.tag <= self.value {
+                        lab.textColor = componentActiveColor
+                        lab.text = "★"
+                    }
+                    else {
+                        lab.textColor = componentInactiveColor
+                        lab.text = "☆"
+                    }
+                }
+                //label.textColor = componentActiveColor
+                sendActions(for: .valueChanged)
+                label.performFlare()
+            }
+        }
+    }
+}
+
+extension UIView {
+  // "Flare view" animation sequence
+  func performFlare() {
+    func flare()   { transform = CGAffineTransform(scaleX: 1.6, y: 1.6) }
+    func unflare() { transform = .identity }
+    
+    UIView.animate(withDuration: 0.3,
+                   animations: { flare() },
+                   completion: { _ in UIView.animate(withDuration: 0.1) { unflare() }})
+  }
 }
