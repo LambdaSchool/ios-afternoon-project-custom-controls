7ba8c5ae31f4ed957a72fae0b3a3011f87e4da1b
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-01-23 14:03:09 -0800

    initial commit. Basic CustomControl class and ViewController set up

---
+++ Star Rating/Star Rating/View/Custom Controls/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Kenny on 1/23/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    var value: Int = 1
+}
