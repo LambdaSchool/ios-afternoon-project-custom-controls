1163363269698c9faae1d40a8a7b21f63043c215
David Wright <davidtwright89@gmail.com>
2020-02-05 02:51:12 -0500

    Created a new Xcode project

---
+++ Star Rating/Star Rating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Rating
+//
+//  Created by David Wright on 2/5/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
