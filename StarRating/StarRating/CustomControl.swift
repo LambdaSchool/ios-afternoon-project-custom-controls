b142b1e4ce11268a93cec74fce03a9523b9c4fbd
Gerardo <gahernandez02@gmail.com>
2020-02-05 22:11:43 -0800

    Connected IBAction

---
+++ StarRating/StarRating/CustomControl.swift
@@ -0,0 +1,18 @@
+//
+//  CustomControl.swift
+//  StarRating
+//
+//  Created by Gerardo Hernandez on 2/5/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class CustomControl: UIControl {
+    
+    // MARK: - Properties
+    
+    private var value: Int = 1
+}
---
+++ StarRating/StarRating/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by Gerardo Hernandez on 2/5/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
