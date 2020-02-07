e354420fe0820363b4b3df66c8ae06f65a6f5028
sal562 <salmanamer@gmail.com>
2020-02-06 18:25:31 -0800

    setup project

---
+++ Star Rating/Star Rating/CustomControl.swift
@@ -0,0 +1,22 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Sal Amer on 2/6/20.
+//  Copyright © 2020 Sal Amer. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+
+    var value: Int = 1
+ 
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+ 
+
+}
