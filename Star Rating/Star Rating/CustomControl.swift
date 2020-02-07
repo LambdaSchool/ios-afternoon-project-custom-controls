de535c6b0c602ddbdf4fc9b3762aa8467034548b
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-02-06 21:15:22 -0500

    Bug fixed to display navigation title. Title does not update.

--- Star Rating/Star Rating/CustomControl.swift
+++ Star Rating/Star Rating/CustomControl.swift
@@ -33,16 +33,6 @@ class CustomControl: UIControl {
     }
     
     
-    
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-    
     // MARK: - Views
     
     func setUpSubviews() {
