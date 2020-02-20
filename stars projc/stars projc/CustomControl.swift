b3c5d9927659fdbd24415db641a47907e718bd98
elizabeth paige <wingateep@Icloud.com>
2020-02-20 18:17:21 -0500

    set up properties + func setup()

--- stars projc/stars projc/CustomControl.swift
+++ stars projc/stars projc/CustomControl.swift
@@ -11,5 +11,43 @@ import UIKit
 
 class CustomControl: UIControl {
     
-    var value = 1 
+    // MARK: - Properties
+    var value = 1
+    
+    let componentDimension: CGFloat = 40
+    let componentCount: Int = 5
+    let componentActiveColor: UIColor = .black
+    let componentInactiveColor: UIColor = .darkGray
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+               setup()
+  }
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
+    
+    // MARK: - Functions
+    func setup() {
+        var spacer: CGFloat = 8.0
+
+        for index in 1...componentCount {
+            let label = UILabel(frame: CGRect(x: spacer, y: 0, width: componentDimension, height: componentDimension))
+            spacer += 8 + componentDimension
+            label.text = "★"
+            label.font = .boldSystemFont(ofSize: 32.0)
+            label.tag = index
+
+            if index == 1 {
+                label.textColor = componentActiveColor
+            }else {
+                label.textColor = componentInactiveColor
+            }
+            self.addSubview(label)
+    }
+  }
 }
--- stars projc/stars projc/StarsViewController.swift
+++ stars projc/stars projc/StarsViewController.swift
@@ -12,25 +12,15 @@ class StarsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+         title = "1 Star"
     }
-    
-    /*
-       // MARK: - Navigation
-
-       // In a storyboard-based application, you will often want to do a little preparation before navigation
-       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           // Get the new view controller using segue.destination.
-           // Pass the selected object to the new view controller.
-       }
-       */
     
     @IBAction func updateRating(_ ratingControl: CustomControl) {
         if ratingControl.value < 2 {
                  self.title = "\(ratingControl.value) star"
-             }else {
+             } else {
                  self.title = "\(ratingControl.value) stars"
-             }
+        }
     }
 }
