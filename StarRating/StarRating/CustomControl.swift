d6225c96da4cfc1f7a26cc9ae40aa61de0db1422
Keri Levesque <kerianne166@gmail.com>
2020-02-20 18:45:53 -0500

    star rating working now

---
+++ StarRating/StarRating/CustomControl.swift
@@ -0,0 +1,123 @@
+//
+//  CustomControl.swift
+//  StarRating
+//
+//  Created by Keri Levesque on 2/20/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//@IBDesignable
+class CustomControl: UIControl {
+    
+    // MARK: - Properties
+    
+    var value: Int = 1
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount = 5
+    private let componentActiveColor: UIColor = .black
+    private let componentInactiveColor: UIColor = .gray
+    private var components: [UILabel] = []
+    
+    //MARK:  View Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubviews()
+    }
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        setUpSubviews()
+    }
+    
+    
+    // MARK: - Views
+    
+    func setUpSubviews() {
+        
+        for i in 1...componentCount {
+            let label = UILabel(frame: CGRect(
+                x: CGFloat(i) * 8 + (CGFloat(i) - 1) * componentDimension,
+                y: 0.0,
+                width: componentDimension,
+                height: componentDimension))
+            label.translatesAutoresizingMaskIntoConstraints = true
+            addSubview(label)
+            label.tag = i
+            components.append(label)
+            
+            label.font = UIFont.systemFont(ofSize: 32)
+            label.textAlignment = .center
+            label.text = "★"
+            if i == 1 {
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
+    
+    // MARK: - Touch Tracking
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        sendActions(for: [.valueChanged])
+        updateValue(at: touch)
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDragInside, .valueChanged])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        if bounds.contains(touch.location(in: self)) {
+            sendActions(for: [.touchDragInside])
+        } else {
+            sendActions(for: [.touchDragOutside])
+            return
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    // MARK: - Change Star Color
+    
+    func updateValue(at touch: UITouch) {
+        for component in components {
+            if component.frame
+                .contains(touch.location(in: self)) {
+                value = component.tag
+                for label in components {
+                    if label.tag <= value {
+                        label.textColor = componentActiveColor
+                    }
+                }
+            } else {
+                for label in components {
+                    if label.tag > value {
+                        label.textColor = componentInactiveColor
+                    }
+                }
+            }
+        }
+    }
+}
--- StarRating/StarRating/ViewController.swift
+++ StarRating/StarRating/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    // MARK: Properties
+    let customControl = CustomControl()
+    
+    //MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        updateRating(customControl)
+     }
 
-
+//MARK: Actions
+    //  will say star instead of stars for only one star rating
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+                switch ratingControl.value {
+           case 1: navigationItem.title = "User Rating: \(ratingControl.value) star"
+           default: navigationItem.title = "User Rating: \(ratingControl.value) stars"
+           }
+      }
 }
 
