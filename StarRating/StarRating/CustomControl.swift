09022866bc3b1ac338b99e06e0d3bf1e17c41f43
Chris <chris.gonzales.82@gmail.com>
2020-02-20 14:47:27 -0800

    TouchHandler methods implemented

--- StarRating/StarRating/CustomControl.swift
+++ StarRating/StarRating/CustomControl.swift
@@ -14,17 +14,94 @@ class CustomControl: UIControl {
     // MARK: - Properties
     
     var value: Int = 1
-    let viewController = ViewController()
+    private var componentDimension: CGFloat = 40.0
+    private var componentCount: CGFloat = 5
+    private var componentActiveColor: UIColor = .black
+    private var componentInactiveColo: UIColor = .gray
+    var labelArray: [UILabel] = []
     
-    @IBAction func updateRating(_ ratingControl: CustomControl) {
-        titleString()
+    // MARK: Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
     }
     
-    private func titleString(){
-        if value == 1 {
-            viewController.title = "User Rating: \(value) star"
-        } else {
-            viewController.title = "User Rating: \(value) stars"
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    // MARK: - Methods
+    
+    func updateValue(at touch: UITouch){
+        
+    }
+
+    // MARK: - Set Up Methods
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
+    
+    private func setup(){
+        
+        frame = CGRect(origin: .zero, size: intrinsicContentSize)
+        
+        
+        for index in 1...Int(componentCount) {
+            let adjustedConstent = CGFloat(index - 1) * componentDimension + CGFloat(index) * 8.0
+            
+            let newLabel = UILabel(frame: CGRect(x: adjustedConstent,
+                                                 y: 0,
+                                                 width: componentDimension,
+                                                 height: componentDimension))
+        addSubview(newLabel)
+            
+            newLabel.tag = index
+            newLabel.font = UIFont.boldSystemFont(ofSize: 32.0)
+            newLabel.text = "⭐️"
+            if newLabel.tag == 1 {
+                newLabel.textColor = componentActiveColor
+            }
+            labelArray.append(newLabel)
         }
     }
+    
+    // MARK: - Touch Handlers
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        sendActions(for: [.touchDown,
+                          .valueChanged])
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint){
+            sendActions(for: [.touchDragInside])
+            updateValue(at: touch)
+        }
+        sendActions(for: [.touchDragOutside])
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint){
+            sendActions(for: [.touchUpInside])
+            updateValue(at: touch)
+        }
+        sendActions(for: [.touchUpOutside])
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: .touchCancel)
+    }
+    
 }
--- StarRating/ViewController.swift
+++ StarRating/ViewController.swift
@@ -12,19 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+  
+    @IBAction func customCell(_ sender: CustomControl) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if sender.value == 1 {
+            self.title = "User Rating: \(sender.value) star"
+            } else {
+            self.title = "User Rating: \(sender.value) stars"
+            }
     }
-    */
-
+    
 }
