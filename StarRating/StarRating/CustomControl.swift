9b8834b9a0566447297d2db3b6195753115c3f89
Tobi Tselios <tobi.tselios@gmail.com>
2020-02-20 21:47:39 +0000

    Create a new custom control class

---
+++ StarRating/StarRating/CustomControl.swift
@@ -0,0 +1,23 @@
+//
+//  CustomControl.swift
+//  StarRating
+//
+//  Created by Tobi Kuyoro on 20/02/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
