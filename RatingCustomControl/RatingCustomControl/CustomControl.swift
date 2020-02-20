ba5acda57aa89d388673df01c4f6fd0395b8f1aa
Nick Nguyen <ptnguyen1901@gmail.com>
2020-02-20 17:49:09 -0500

    Added five labels

--- RatingCustomControl/RatingCustomControl/CustomControl.swift
+++ RatingCustomControl/RatingCustomControl/CustomControl.swift
@@ -10,15 +10,66 @@ import UIKit
 
 class CustomControl: UIControl {
 
-    var value : Int  = 1 
+    var value : Int  = 1
     
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
     }
-    */
+    
+    private let componentDimension : CGFloat = 40.0
+    
+    private let componentCount  = 5
+    
+    private let componentActiveColor : UIColor = .black
+    
+    private let componentInActiveColor : UIColor = .gray
+    
+    var labels : [UILabel] = []
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        for i in 1 ... 5 {
+            let label = UILabel()
+          
+            label.tag = i
+            label.frame.size  = CGSize(width: componentDimension, height: componentDimension)
+            label.font = UIFont.boldSystemFont(ofSize: 32)
+            label.text = "⭐️"
+            label.textAlignment = .center
+            label.textColor = label.tag == 1 ? componentActiveColor : componentInActiveColor
+            switch label.tag {
+            case 1:
+                label.frame.origin = CGPoint(x: 8.0, y: 0)
+            case 2:
+                label.frame.origin = CGPoint(x: componentDimension + 16.0, y: 0)
+            case 3:
+                label.frame.origin = CGPoint(x: componentDimension + 64.0, y: 0)
+            case 4:
+                label.frame.origin = CGPoint(x: componentDimension + 112.0, y: 0)
+            case 5:
+                label.frame.origin = CGPoint(x: componentDimension + 160.0, y: 0)
+            default:
+                break
+            }
+            addSubview(label)
+            labels.append(label)
+            
+        }
+        
+    }
+    
+   
 
 }
