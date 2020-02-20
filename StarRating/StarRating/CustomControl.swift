21bc9dd5f168154a5a498dce2a7d44d1960bce59
Enrique Gongora <EGTech24@gmail.com>
2020-02-20 15:26:45 -0800

    Control View - Stars are appearing correctly

--- StarRating/StarRating/CustomControl.swift
+++ StarRating/StarRating/CustomControl.swift
@@ -10,6 +10,41 @@ import Foundation
 import UIKit
 
 class CustomControl: UIControl {
-    var value: Int = 1
     
+    //MARK: - Variables
+    var value: Int = 1
+    var componentDimension: CGFloat = 40.0
+    var componentCount = 5
+    var componentActiveColor = UIColor.black
+    var componentInactiveColor = UIColor.gray
+    var starArray: [UILabel] = []
+    
+    // MARK: - Initializers
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    //MARK: - Functions
+    func setup() {
+        for number in 1...componentCount {
+            let label = UILabel(frame: CGRect(x: CGFloat(number) * 8 + (CGFloat(number) - 1) * componentDimension, y: 0.0, width: componentDimension, height: componentDimension))
+            label.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(label)
+            label.tag = number
+            label.font = UIFont.boldSystemFont(ofSize: 32.0)
+            label.text = (number == 1) ? "🌟" : "☆"
+            label.textAlignment = .center
+            label.textColor = (number == 1) ? componentActiveColor : componentInactiveColor
+            starArray.append(label)
+        }
+    }
+    
+    // This tells Auto Layout how big our custom control should be
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
 }
