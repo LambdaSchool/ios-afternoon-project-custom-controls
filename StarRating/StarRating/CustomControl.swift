bc310b86a1582dcc7ce620a9d56af5359cfa44bd
Tobi Tselios <tobi.tselios@gmail.com>
2020-02-21 00:07:27 +0000

    MVP

--- StarRating/StarRating/CustomControl.swift
+++ StarRating/StarRating/CustomControl.swift
@@ -21,6 +21,11 @@ class CustomControl: UIControl {
     
     // MARK: - View Lifecycle
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setup()
@@ -28,18 +33,14 @@ class CustomControl: UIControl {
     
     func setup() {
         for i in 1...componentCount {
-            let label = UILabel()
+            let label = UILabel(frame: CGRect(x: CGFloat(i) * 8 + (CGFloat(i) - 1) * componentDimension,
+                                              y: 0.0,
+                                              width: componentDimension,
+                                              height: componentDimension))
             label.tag = i
             label.textAlignment = .center
-            label.font = .systemFont(ofSize: 32, weight: .bold)
+            label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
             label.text = "⭑"
-            label.textColor = componentActiveColor
-            label.translatesAutoresizingMaskIntoConstraints = false
-            
-            let offset = CGFloat(i - 1) * componentDimension + CGFloat(i) * 8.0
-            let origin = CGPoint(x: offset, y: 0)
-            let size = CGSize(width: componentDimension, height: componentDimension)
-            label.frame = CGRect(origin: origin, size: size)
             
             if label.tag == 1 {
                 label.textColor = componentActiveColor
@@ -57,5 +58,85 @@ class CustomControl: UIControl {
         let componentsSpacing = CGFloat(componentCount + 1) * 8.0
         let width = componentsWidth + componentsSpacing
         return CGSize(width: width, height: componentDimension)
+    }
+    
+    // MARK: - Touch Tracking
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        sendActions(for: [.valueChanged])
+        updateValue(at: touch)
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDragInside, .valueChanged])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        if bounds.contains(touch.location(in: self)) {
+            sendActions(for: [.touchDragInside])
+        } else {
+            sendActions(for: [.touchDragOutside])
+            return
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    // MARK: - Change Star Color
+    
+    func updateValue(at touch: UITouch) {
+        let touchPoint = touch.location(in: self)
+        for label in labels {
+            if label.frame.contains(touchPoint) {
+                value = label.tag
+                sendActions(for: [.valueChanged])
+            }
+        }
+        switch value {
+        case 1:
+            labels[0].textColor = componentActiveColor
+            labels[1].textColor = componentInactiveColor
+            labels[2].textColor = componentInactiveColor
+            labels[3].textColor = componentInactiveColor
+            labels[4].textColor = componentInactiveColor
+        case 2:
+            labels[0].textColor = componentActiveColor
+            labels[1].textColor = componentActiveColor
+            labels[2].textColor = componentInactiveColor
+            labels[3].textColor = componentInactiveColor
+            labels[4].textColor = componentInactiveColor
+            
+        case 3:
+            labels[0].textColor = componentActiveColor
+            labels[1].textColor = componentActiveColor
+            labels[2].textColor = componentActiveColor
+            labels[3].textColor = componentInactiveColor
+            labels[4].textColor = componentInactiveColor
+        case 4:
+            labels[0].textColor = componentActiveColor
+            labels[1].textColor = componentActiveColor
+            labels[2].textColor = componentActiveColor
+            labels[3].textColor = componentActiveColor
+            labels[4].textColor = componentInactiveColor
+        case 5:
+            labels[0].textColor = componentActiveColor
+            labels[1].textColor = componentActiveColor
+            labels[2].textColor = componentActiveColor
+            labels[3].textColor = componentActiveColor
+            labels[4].textColor = componentActiveColor
+        default:
+            break
+        }
     }
 }
