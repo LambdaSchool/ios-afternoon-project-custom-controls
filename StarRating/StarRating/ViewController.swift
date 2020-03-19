37403dcdf11ebde5cfb91efedf0ece3a3ca44d6e
Hector L <hectoremil21@hotmail.com>
2020-03-19 19:46:42 -0400

    Create custom button programmatically.

--- StarRating/StarRating/ViewController.swift
+++ StarRating/StarRating/ViewController.swift
@@ -9,16 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var customControl: CustomControl!
+    
+    private let changeDirection = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "User Rating: 1 star."
+        setUpButton()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func updateRating(_ ratingControl: CustomControl) {
         self.title = ratingControl.value == 1 ? "User Rating: \(ratingControl.value) star" : "User Rating: \(ratingControl.value) stars"
         
+    }
+    
+    private func setUpButton() {
+        changeDirection.translatesAutoresizingMaskIntoConstraints = false
+        changeDirection.setTitle("Change direction.", for: .normal)
+        changeDirection.setTitleColor(.black, for: .normal)
+        view.addSubview(changeDirection)
+        
+        NSLayoutConstraint.activate([
+            changeDirection.leadingAnchor.constraint(equalTo: customControl.leadingAnchor),
+            changeDirection.trailingAnchor.constraint(equalTo: customControl.trailingAnchor),
+            changeDirection.topAnchor.constraint(equalTo: customControl.bottomAnchor, constant: 20)
+        ])
     }
     
 }
