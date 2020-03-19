5d7cb68b507c49087818ff64e04dc8c74fcc01d9
Christopher DeVito <toolofjesus@protonmail.com>
2020-03-19 18:14:25 -0400

    Custom Controls MVP + Stretch - fit the title to display star or stars appropriately, and Stretch - flare animation.

--- StarRating/StarRating/Controlls/CustomControl.swift
+++ StarRating/StarRating/Controlls/CustomControl.swift
@@ -18,7 +18,9 @@ class CustomControl: UIControl {
     private let componentCount = 5
     private let componentActiveColor = UIColor.black
     private let componentInactiveColor = UIColor.gray
+    var ratingArray: [UILabel] = []
     
+    // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -29,9 +31,10 @@ class CustomControl: UIControl {
         setup()
     }
     
+    // MARK: - View lifecycle
     func setup() {
         //backgroundColor = .clear
-        var ratingArray: [UILabel] = []
+        
         for i in 1...5 {
             let label = UILabel()
             addSubview(label)
@@ -59,4 +62,72 @@ class CustomControl: UIControl {
         let width = componentsWidth + componentsSpacing
         return CGSize(width: width, height: componentDimension)
     }
+    
+    func updateValue(at touch: UITouch) {
+        for label in ratingArray {
+            if label.frame.contains(touch.location(in: self)) {
+                CustomControl.value = label.tag
+                for i in 0..<ratingArray.count {
+                    if ratingArray[i].tag <= CustomControl.value {
+                        ratingArray[i].textColor = componentActiveColor
+                    } else {
+                        ratingArray[i].textColor = componentInactiveColor
+                    }
+                    sendActions(for: .valueChanged)
+                }
+            }
+        }
+    }
+    
+    // MARK: - Actions
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        performFlare()
+        sendActions(for: [.touchDown, .valueChanged])
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if self.bounds.contains(touchPoint) {
+            let storedValue = CustomControl.value
+            updateValue(at: touch)
+            if storedValue != CustomControl.value {
+                performFlare()
+            }
+            sendActions(for: [.touchDragInside, .touchDragOutside, .valueChanged])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: self)
+        if self.bounds.contains(touchPoint) {
+            updateValue(at: touch)
+            sendActions(for: [.touchUpInside, .touchUpOutside, .valueChanged])
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
 }
+
+extension UIView {
+    // Flare view animation sequence
+    func performFlare() {
+        func flare() { transform = CGAffineTransform(scaleX: 1.6, y: 1.6)}
+        func unflare() { transform = .identity}
+        
+        UIView.animate(withDuration: 0.3,
+                       animations: { flare() },
+                       completion: { _ in UIView.animate(withDuration: 0.1) { unflare() }})
+    }
+}
+
