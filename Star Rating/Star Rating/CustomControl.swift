78b151bdc2bc48b33ec7c7d4e20b040beb5a5a6c
Nichole Davidson <nicholedavid@gmail.com>
2020-03-19 13:44:49 -0600

    Initial Setup

---
+++ Star Rating/Star Rating/CustomControl.swift
@@ -0,0 +1,17 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Nichole Davidson on 3/19/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// This property is API-facing, clients will be able to see it.
+// Establishes your control as a value-providing (and value-changing) type.
+class CustomControl: UIControl {
+    var valie: Int = 1
+    
+}
