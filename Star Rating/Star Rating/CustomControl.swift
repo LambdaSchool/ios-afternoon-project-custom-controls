6f6190d8f85bdfd7354bd5b14318b38a9b0223b0
Mark Gerrior <github@gerrior.com>
2020-03-19 13:33:51 -0700

    Create a new custom control class: Steps 1-3

---
+++ Star Rating/Star Rating/CustomControl.swift
@@ -0,0 +1,14 @@
+//
+//  CustomControl.swift
+//  Star Rating
+//
+//  Created by Mark Gerrior on 3/19/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomControl {
+    var value: Int = 1
+}
