04c5ffda65e71281243f609c82c4de2de72eac5b
Shawn Gee <shawngee@gmail.com>
2020-03-19 17:17:16 -0400

    Stars displaying properly

--- StarRating/StarRating/CustomControl.swift
+++ StarRating/StarRating/CustomControl.swift
@@ -10,14 +10,91 @@ import UIKit
 
 class CustomControl: UIControl {
 
+    // MARK: - Properties
     
     var value = 1
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Private
+
+    let componentDimension: CGFloat = 40
+    let componentPadding: CGFloat = 8
+    let componentCount = 5
+    let componentColor = UIColor.systemPink
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        var stars = [UIImageView]()
+        
+        var x = componentPadding
+        
+        for i in 1...5 {
+            let starImage = i == 1 ? UIImage(systemName: "star.fill") : UIImage(systemName: "star")
+            let star = UIImageView(image: starImage)
+            star.tintColor = componentColor
+            star.contentMode = .scaleAspectFit
+            star.tag = i
+            
+            stars.append(star)
+            
+            addSubview(star)
+            star.frame = CGRect(x: x, y: 0, width: componentDimension, height: componentDimension)
+            
+            x += componentDimension + componentPadding
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
+    
+    //private func update
+    
+    // MARK: - Touch Tracking
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        sendActions(for: [.touchDown, .valueChanged])
+        return true
+    }
+    
+//      override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+//          let touchPoint = touch.location(in: colorWheel)
+//          if colorWheel.bounds.contains(touchPoint) {
+//              color = colorWheel.color(for: touchPoint)
+//              sendActions(for: [.touchDragInside, .valueChanged])
+//          } else {
+//              sendActions(for: .touchDragOutside)
+//          }
+//
+//          return true
+//      }
+//
+//      override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+//          guard let touch = touch else { return }
+//
+//          let touchPoint = touch.location(in: colorWheel)
+//          if colorWheel.bounds.contains(touchPoint) {
+//              color = colorWheel.color(for: touchPoint)
+//              sendActions(for: [.touchUpInside, .valueChanged])
+//          } else {
+//              sendActions(for: .touchUpOutside)
+//          }
+//      }
+//
+//      override func cancelTracking(with event: UIEvent?) {
+//          sendActions(for: .touchCancel)
+//      }
 
 }
