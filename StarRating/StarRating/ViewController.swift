6da5dd076245ca02e761cf38a883ff846b5272fa
Hector L <hectoremil21@hotmail.com>
2020-03-19 15:34:44 -0400

    Initial Setup

---
+++ StarRating/StarRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by Karen Rodriguez on 3/19/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
