d42d3be10fbdcc9a568e41c8a3c56134b1b3033c
Mark Gerrior <github@gerrior.com>
2020-03-19 14:05:42 -0700

    Build the control view: Steps 1 & 2

--- Star Rating/Star Rating/CustomControl.swift
+++ Star Rating/Star Rating/CustomControl.swift
@@ -11,4 +11,18 @@ import UIKit
 
 class CustomControl: UIControl {
     var value: Int = 1
+    
+    let componentDimension: CGFloat = 40.0
+    let componentCount = 5
+    let componentActiveColor = UIColor.black
+    let componentInactiveColor = UIColor.gray
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        
+    }
 }
