5acbb82d956575bc9379a758cb7d5c4b3d6d9042
Breena <breena_greek@live.com>
2020-04-16 18:17:52 -0400

    Initial Commit

---
+++ UICustomControl/UICustomControl/Views/CustomControl.swift
@@ -0,0 +1,18 @@
+//
+//  CustomControl.swift
+//  UICustomControl
+//
+//  Created by Breena Greek on 4/16/20.
+//  Copyright © 2020 Breena Greek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomControl: UIControl {
+    
+    // MARK: - Properties
+    var value: Int = 1
+    
+    
+}
---
+++ UICustomControl/UICustomControl/Views/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UICustomControl
+//
+//  Created by Breena Greek on 4/16/20.
+//  Copyright © 2020 Breena Greek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - IBAction
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
