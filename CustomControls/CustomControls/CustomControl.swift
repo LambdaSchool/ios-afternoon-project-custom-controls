3553e97957e939111147066642f4dc5c66c6aba0
Cameron Collins <cameroncollins19@yahoo.com>
2020-04-16 14:58:20 -0500

    Added Custom Control Class

---
+++ CustomControls/CustomControls/CustomControl.swift
@@ -0,0 +1,17 @@
+//
+//  CustomControl.swift
+//  CustomControls
+//
+//  Created by Cameron Collins on 4/16/20.
+//  Copyright © 2020 Cameron Collins. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    //MARK: - Variables
+    var value: Int = 1
+    
+    
+}
