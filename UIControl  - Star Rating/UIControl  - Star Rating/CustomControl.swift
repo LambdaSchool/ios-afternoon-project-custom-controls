c15b6a8146e16346bb1abec7ed0467bb0423384f
Mark Poggi <markp@mac.com>
2020-04-16 11:27:27 -0700

    Set up initial project

---
+++ UIControl  - Star Rating/UIControl  - Star Rating/CustomControl.swift
@@ -0,0 +1,20 @@
+//
+//  CustomControl.swift
+//  UIControl  - Star Rating
+//
+//  Created by Mark Poggi on 4/16/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+    
+    
+    
+    
+    
+}
---
+++ UIControl  - Star Rating/UIControl  - Star Rating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIControl  - Star Rating
+//
+//  Created by Mark Poggi on 4/16/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        
+
+    }
+    
+}
+
