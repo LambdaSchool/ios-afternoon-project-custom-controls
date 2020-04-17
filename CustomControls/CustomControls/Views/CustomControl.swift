1edc6b6be2a821b621b65f1199f66667104bc27d
Matt Martindale <martindalematt18@yahoo.com>
2020-04-16 19:09:31 -0600

    added touch methods

--- CustomControls/CustomControls/Views/CustomControl.swift
+++ CustomControls/CustomControls/Views/CustomControl.swift
@@ -16,6 +16,8 @@ class CustomControl: UIControl {
     private let componentActiveColor: UIColor = #colorLiteral(red: 0.08015998453, green: 0.2788668573, blue: 0.3865089417, alpha: 1)
     private let componentInactiveColor: UIColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
     
+    var labelArray: [UILabel] = []
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setUp()
@@ -35,7 +37,7 @@ class CustomControl: UIControl {
         stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         
-        var labelArray: [UILabel] = []
+        
         var labelTag = 0
         for _ in 1...5 {
             let label = UILabel()
@@ -43,7 +45,7 @@ class CustomControl: UIControl {
             label.tag = labelTag
             
             label.text = "★"
-            label.textColor = componentActiveColor
+            label.textColor = componentInactiveColor
             addSubview(label)
             labelArray.append(label)
             
@@ -54,4 +56,68 @@ class CustomControl: UIControl {
             stackView.addArrangedSubview(label)
         }
     }
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
+    
+    func updateValue(at touch: UITouch) {
+        let touchPoint = touch.location(in: self)
+        for label in labelArray {
+            if label.bounds.contains(touchPoint) {
+                value = label.tag
+                label.textColor = componentActiveColor
+                sendActions(for: .valueChanged)
+            }
+        }
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        
+        updateValue(at: touch)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDown])
+        }
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDragInside, .touchDragOutside])
+            updateValue(at: touch)
+        } else {
+            sendActions(for: .touchDragOutside)
+        }
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer {
+            super.endTracking(touch, with: event)
+        }
+        
+        guard let touch = touch else { return }
+        
+        let touchPoint = touch.location(in: self)
+        
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchUpInside, .touchUpOutside])
+        } else {
+            sendActions(for: .touchDragOutside)
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: .touchCancel)
+        super.cancelTracking(with: event)
+    }
+    
+    
 }
