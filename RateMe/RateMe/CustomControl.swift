2aa79ae4baef5ffb3c59c9b86e2a056912042649
kensam1 <samarakodykennedy@gmail.com>
2020-04-16 23:45:15 -0400

    Began Custom Control project

---
+++ RateMe/RateMe/CustomControl.swift
@@ -0,0 +1,20 @@
+//
+//  CustomControl.swift
+//  RateMe
+//
+//  Created by Lambda_School_Loaner_241 on 4/16/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomControl: UIControl {
+    var value: Int = 1
+    
+    
+    
+}
+
+
+// number 6
---
+++ RateMe/RateMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RateMe
+//
+//  Created by Lambda_School_Loaner_241 on 4/16/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
