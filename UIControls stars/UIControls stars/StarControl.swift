faf5a165aca3a95c6064862104091ee110dd695a
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-04-16 21:55:01 -0700

    Added properties to shape

--- UIControls stars/UIControls stars/StarControl.swift
+++ UIControls stars/UIControls stars/StarControl.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class StarControl: UIControl {
 
+    var value: Int = 1
+      private let componentDimension: CGFloat = 40.0
+      private let componentCount = 5
+      private let componentActiveColor: UIColor = .red
+      private let componentInactiveColor: UIColor = .gray
+      private var stars: [UILabel] = []
+      
+      override init(frame: CGRect) {
+          super.init(frame: frame)
+          setup()
+      }
+      
+      required init?(coder: NSCoder) {
+          super.init(coder: coder)
+          setup()
+      }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
