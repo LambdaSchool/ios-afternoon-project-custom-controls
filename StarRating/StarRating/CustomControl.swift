5b40846795a618bcc0713efd58e1036f28a01d11
Chad Parker <chad@chadparker.com>
2020-04-16 19:09:17 -0700

    Add HomeViewController and CustomControl

---
+++ StarRating/StarRating/CustomControl.swift
@@ -0,0 +1,21 @@
+//
+//  CustomControl.swift
+//  StarRating
+//
+//  Created by Chad Parker on 4/16/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+    
+    private let componentDimension: CGFloat = 40
+    private let componentCount = 5
+    private let componentActiveColor: UIColor = .black
+    private let componentInactiveColor: UIColor = .gray
+    
+    
+}
