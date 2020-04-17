491f27ff43f11ad20c036056ae7e6eddafe34560
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-17 00:37:04 -0400

    Not MVP

--- Rating/Rating/CustomControl.swift
+++ Rating/Rating/CustomControl.swift
@@ -11,24 +11,128 @@ import UIKit
 
 class CustomControl: UIControl {
     
-    var value: Int = 1
+    var value: Int = 1 {
+        didSet {
+            if value != value {
+                updateStars()
+                sendActions(for: .valueChanged)
+            }
+        }
+    }
     
     private let componentDimension: CGFloat = 40.0
-    private let componentCount: Int = 5
+    private let componentCount = 5
     private let componentActiveColor = UIColor.black
     private let componentInactiveColor = UIColor.gray
+    var labelArray = [UILabel]()
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         setup()
-        
+    }
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        setup()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
     }
     
     func setup() {
         
-        var labelArray = [UILabel]()
+        let spacing = 8
+        var x = spacing
         
+        for i in 1...componentCount {
+            let label = UILabel()
+            labelArray.append(label)
+            label.tag = i
+            
+            label.frame = CGRect(x: CGFloat(x),
+                                 y: 0,
+                                 width: componentDimension,
+                                 height: componentDimension)
+            
+            x += Int(componentDimension) + spacing
+            
+            label.font = UIFont.boldSystemFont(ofSize: 32.0)
+            label.text = "★"
+            label.textAlignment = .center
+            if i <= value {
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
+            addSubview(label)
+            
+        }
+    }
+    
+    // MARK: Touch Handlers
+    
+    func updateStars() {
         
+        for i in 0...labelArray.count {
+            if labelArray[i].tag <= value {
+                labelArray[i].textColor = componentActiveColor
+                print("active")
+            } else {
+                labelArray[i].textColor = componentInactiveColor
+                print("inactive")
+            }
+        }
+        sendActions(for: .valueChanged)
         
+    }
+    
+
+    func updateValue(at touch: UITouch) {
+        for i in 0..<labelArray.count {
+            let touchPoint = touch.location(in: labelArray[i])
+            if labelArray[i].bounds.contains(touchPoint) {
+                value = i + 1
+                
+                sendActions(for: .valueChanged)
+                
+                break
+            }
+        }
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        sendActions(for: [.touchDown])
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            updateValue(at: touch)
+            sendActions(for: [.touchDragInside])
+        } else {
+            sendActions(for: .touchDragOutside)
+        }
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            updateValue(at: touch)
+            sendActions(for: [.touchUpInside])
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
     }
 }
--- Rating/Rating/ViewController.swift
+++ Rating/Rating/ViewController.swift
@@ -24,11 +24,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         
     }
-
-    
-
 }
 
