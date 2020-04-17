b68ffac646cb3fc2951d307acc11bbf481a81f40
Claudia Maciel <clc_80@yahoo.com>
2020-04-16 17:49:34 -0700

    Added new project

---
+++ CustomControls/CustomControls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomControls
+//
+//  Created by Claudia Contreras on 4/16/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
