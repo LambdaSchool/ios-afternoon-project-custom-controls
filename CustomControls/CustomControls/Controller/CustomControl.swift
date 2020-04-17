fc7961f01a985611c72ac5d8a87f98833b6f52ae
Claudia Maciel <clc_80@yahoo.com>
2020-04-16 18:15:04 -0700

    Created and connected CustomControl class

---
+++ CustomControls/CustomControls/Controller/CustomControl.swift
@@ -0,0 +1,24 @@
+//
+//  CustomControl.swift
+//  CustomControls
+//
+//  Created by Claudia Contreras on 4/16/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CustomControls/CustomControls/View Controllers/ViewController.swift
+++ CustomControls/CustomControls/View Controllers/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func updateRating(_ ratingControl: CustomControl) {
+        if ratingControl.value == 1 {
+            title = "User Rating: \(ratingControl.value) star"
+        } else {
+            title = "User Rating: \(ratingControl.value) stars"
+        }
+     
+    }
+    
 }
 
