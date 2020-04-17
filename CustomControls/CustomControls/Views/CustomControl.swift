2d092dcc4a6c582b238fc346728ea8fcc7da511c
Matt Martindale <martindalematt18@yahoo.com>
2020-04-16 18:20:07 -0600

    Setup labels

--- CustomControls/CustomControls/Views/CustomControl.swift
+++ CustomControls/CustomControls/Views/CustomControl.swift
@@ -13,8 +13,8 @@ class CustomControl: UIControl {
     var value: Int = 1
     private let componentDimension: CGFloat = 40.0
     private let componentCount = 5
-    private let componentActiveColor: UIColor = .black
-    private let componentInactiveColor: UIColor = .gray
+    private let componentActiveColor: UIColor = #colorLiteral(red: 0.08015998453, green: 0.2788668573, blue: 0.3865089417, alpha: 1)
+    private let componentInactiveColor: UIColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -22,14 +22,36 @@ class CustomControl: UIControl {
     }
     
     func setUp() {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
         var labelArray: [UILabel] = []
         var labelTag = 0
         for _ in 1...5 {
             let label = UILabel()
             labelTag += 1
             label.tag = labelTag
+            
+            label.text = "★"
+            label.textColor = componentActiveColor
             addSubview(label)
             labelArray.append(label)
+            
+            label.frame.size.width = componentDimension
+            label.frame.size.height = componentDimension
+            label.font = UIFont.boldSystemFont(ofSize: 32)
+            
+            stackView.addArrangedSubview(label)
         }
     }
 }
