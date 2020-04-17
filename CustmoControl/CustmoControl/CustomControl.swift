9a40008d1648567d3594352b298a81aaaf3a7c34
Harmony Radley <harmonyradley99@gmail.com>
2020-04-16 21:41:57 -0400

    Followed steps for Create a new custom control class

---
+++ CustmoControl/CustmoControl/CustomControl.swift
@@ -0,0 +1,22 @@
+//
+//  CustomControl.swift
+//  CustmoControl
+//
+//  Created by Harmony Radley on 4/16/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+
+class CustomControl: UIControl {
+    
+    var value: Int = 1
+    
+    
+    
+    
+    
+    
+}
