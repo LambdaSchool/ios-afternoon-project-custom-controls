ded24fc17070322d9528bd0f6b2bed65de81299f
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-04-16 21:53:13 -0700

    Project started

---
+++ UIControls stars/UIControls stars/StarControl.swift
@@ -0,0 +1,21 @@
+//
+//  StarControl.swift
+//  UIControls stars
+//
+//  Created by Bhawnish Kumar on 4/16/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class StarControl: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ UIControls stars/UIControls stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIControls stars
+//
+//  Created by Bhawnish Kumar on 4/16/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
