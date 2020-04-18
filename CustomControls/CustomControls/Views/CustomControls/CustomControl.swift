7a5c7131bba9a2bedbd33bb22be7938b3e324b06
James McDougall <jameswiliammcdougall@gmail.com>
2020-04-17 22:58:00 -0700

    Created View Controllerr, Navigation Controller and centered UI View

---
+++ CustomControls/CustomControls/Views/CustomControls/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  CustomControls
+//
+//  Created by Lambda_School_loaner_226 on 4/17/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl {
+    var value: Int = 1
+}
