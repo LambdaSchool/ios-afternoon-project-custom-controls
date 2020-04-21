28da189c598e54d8c198919f903689ac3248b34c
David Williams <burmesedragon@att.net>
2020-04-20 20:14:48 -0700

    just experimenting but do not think on the right track

--- ExpandtheNumber.playground/Contents.swift
+++ ExpandtheNumber.playground/Contents.swift
@@ -1,10 +1,20 @@
 import UIKit
 
+var numbers: [String] = []
+
 func expandTheNumber(num: Int) {
-    
-    let amt = String(num).count
-    for a in 1...amt {
-        let character
-    }
+    let characters = String(num)
+    let amt = characters.count
+    let first = characters.prefix(1)
+    let firstConverted = Int(first)
+    let firstMultiplied = firstConverted! * (amt)
+    print(firstMultiplied)
+    print(firstConverted)
+    print(first)
+//    for a in 1...amt {
+//        print(characters[a])
+//       // numbers.append(characters[a])
+//    }
     
 }
+ expandTheNumber(num: 450)
