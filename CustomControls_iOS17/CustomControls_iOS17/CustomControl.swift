dad41ba75879fcf44249d37b02bc64c5a3599975
Stephanie Ballard <user.name>
2020-05-14 13:17:00 -0700

    control view built

--- CustomControls_iOS17/CustomControls_iOS17/CustomControl.swift
+++ CustomControls_iOS17/CustomControls_iOS17/CustomControl.swift
@@ -10,7 +10,52 @@ import UIKit
 
 class CustomControl: UIControl {
     
+    // MARK: - Properties -
     var value: Int = 1
     
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount = 5
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
+    private var components: [UILabel] = []
     
+    // MARK: - Initializers -
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        setup()
+    }
+    
+    func setup() {
+        for i in 1...componentCount {
+            let label = UILabel(frame: CGRect(x: CGFloat(i) * 8 + (CGFloat(i) - 1) * componentDimension,
+                                              y: 0.0,
+                                              width: componentDimension,
+                                              height: componentDimension))
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.tag = 1
+            addSubview(label)
+            components.append(label)
+            label.font = UIFont.systemFont(ofSize: 32)
+            label.textAlignment = .center
+            label.text = "★"
+            
+            if i == 1 {
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let componentsWidth = CGFloat(componentCount) * componentDimension
+        let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+        let width = componentsWidth + componentsSpacing
+        return CGSize(width: width, height: componentDimension)
+    }
 }
