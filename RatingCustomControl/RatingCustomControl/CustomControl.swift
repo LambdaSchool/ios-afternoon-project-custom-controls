f64a12f23b87fd9310f87cc11d820b86aea11fc9
Clayton Watkins <watkinclayton17@yahoo.com>
2020-05-28 20:36:02 -0700

    Created and added labels via a for loop

--- RatingCustomControl/RatingCustomControl/CustomControl.swift
+++ RatingCustomControl/RatingCustomControl/CustomControl.swift
@@ -11,6 +11,55 @@ import UIKit
 
 class CustomControl: UIControl {
     
+    // MARK: - Properties
     var value: Int = 1
+    private let componentDimension: CGFloat = 40.0
+    private let componentCount = 5
+    private let componentActiveColor = UIColor.black
+    private let componentInactiveColor = UIColor.gray
+    var starLabels: [UILabel] = []
     
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder){
+        super.init(coder: coder)
+        setUpViews()
+    }
+    
+    // MARK: - Setting Up Views
+    private func setUpViews(){
+        
+        for n in 1...componentCount {
+            let label = UILabel()
+            label.tag = n
+            starLabels.append(label)
+            addSubview(label)
+            label.font = UIFont.boldSystemFont(ofSize: 32.0)
+            label.text = "☆"
+            label.textAlignment = .center
+            
+            if n == 1{
+                label.textColor = componentActiveColor
+            } else {
+                label.textColor = componentInactiveColor
+            }
+            
+            let padding = CGFloat(8.0 * CGFloat(n))
+            label.frame = CGRect(x: 64 + padding + (componentDimension * CGFloat(n-1)),
+                                 y: 80,
+                                 width: componentDimension,
+                                 height: componentDimension)
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+      let componentsWidth = CGFloat(componentCount) * componentDimension
+      let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+      let width = componentsWidth + componentsSpacing
+      return CGSize(width: width, height: componentDimension)
+    }
 }
